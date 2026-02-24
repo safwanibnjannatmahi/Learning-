c2f9c5e7b83efb78073d8969b0d4f0606e9c9c76
Safwan Mahi <safwanibnjannat@gmail.com>
2026-02-24 10:03:55 +0600

    Update 1st_class.c

--- DATA STRUCTURE LAB B1/1st_class.c
+++ DATA STRUCTURE LAB B1/1st_class.c
@@ -20,9 +20,9 @@ int main(){
     else
         printf("item not found");
 
-    for (i=0; j< 5-1; i++)
+    for (i=0; i< 5-1; i++)
     {
-        for(j=0;j< 5-j-1;j++ ){
+        for(j=0;j< 5-i-1;j++ ){
             if (arr[j]> arr[j+1])
             {
                 int temp= arr[j];
